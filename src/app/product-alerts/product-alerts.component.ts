9558097589182b1acbc208f90a9cc7de369f2063
difly <kryshen.yaroslav@gmail.com>
2019-09-08 11:33:04 +0300

    Add component app-product-alerts to view in product-list

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
-  selector: 'app-product-alets',
+  selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
 
 export class ProductAlertsComponent implements OnInit {
+  @Input() product;
+
   constructor(){}
 
   ngOnInit(){
-    
+
   }
 }
